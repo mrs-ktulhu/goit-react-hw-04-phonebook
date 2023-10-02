a465135dfec8bfb111abdbe9cb539a78f7b7b573
Mamba <113925095+mrs-ktulhu@users.noreply.github.com>
2023-10-02 19:30:23 +0300

    add useState for Contact Form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,68 +1,77 @@
-import React, { Component } from 'react';
+import {useState} from 'react';
 import { nanoid } from 'nanoid';
 import { Container, FormInput, SubmitButton } from './ContactForm.styled';
 
-export default class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+export default function ContactForm() {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = e => {
+    const {name, value} = e.target;
+    switch(name){
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleChange = e => {
-    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
-  };
-
-
-handleSubmit = e => {
-  e.preventDefault();
-
-  const { name, number } = this.state;
-
-  if (this.props.contacts.some(contact => contact.text === name)) {
-    alert(`${name} is already in contacts.`);
-    this.setState({ name: '' }); 
-  } else {
-    this.props.onSubmit(name, number);
-    this.setState({ name: '', number: '' }); 
-  }
-};
-
-
-  render() {
-    return (
-      <Container>
-        <form onSubmit={this.handleSubmit}>
-          <FormInput>
-            Name
-            <br />
-            <input
-              type="text"
-              placeholder="Enter name"
-              name="name"
-              id={nanoid()}
-              value={this.state.name}
-              onChange={this.handleChange}
-              required
-            />
-          </FormInput>
+  return (
+    <Container>
+      <form >
+        <FormInput>
+          Name
           <br />
-          <FormInput>
-            Number
-            <br />
-            <input
-              type="tel"
-              placeholder="Enter number"
-              name="number"
-              id={nanoid()}
-              value={this.state.number}
-              onChange={this.handleChange}
-              required
-            />
-          </FormInput>
+          <input
+            type="text"
+            placeholder="Enter name"
+            name="name"
+            id={nanoid()}
+            value={name}
+            onChange={handleChange}
+            required
+          />
+        </FormInput>
+        <br />
+        <FormInput>
+          Number
           <br />
-          <SubmitButton type="submit">Add contact</SubmitButton>
-        </form>
-      </Container>
-    );
-  }
+          <input
+            type="tel"
+            placeholder="Enter number"
+            name="number"
+            id={nanoid()}
+            value={number}
+            onChange={handleChange}
+            required
+          />
+        </FormInput>
+        <br />
+        <SubmitButton type="submit">Add contact</SubmitButton>
+      </form>
+    </Container>
+  );
 }
+
+// // export default class ContactForm extends Component {
+
+// handleSubmit = e => {
+//   e.preventDefault();
+
+//   const { name, number } = this.state;
+
+//   if (this.props.contacts.some(contact => contact.text === name)) {
+//     alert(`${name} is already in contacts.`);
+//     this.setState({ name: '' });
+//   } else {
+//     this.props.onSubmit(name, number);
+//     this.setState({ name: '', number: '' });
+//   }
+// };
+//     );
+//   }
+// // }
